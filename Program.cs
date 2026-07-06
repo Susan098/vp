e75102f7c78ce38aa42e771407154915eab111d6
Susan <susansubedi307@gmail.com>
2026-07-06 15:34:02 +0545

    update

--- Program.cs
+++ Program.cs
@@ -1,8 +1,22 @@
 ﻿using System;
-class Program
+ public class Program
 {
-    static void Main(string[]args)
+public static void Main(string[] args)
     {
-        Console.Write("hello susan ...");
+        /* Console.WriteLine("Hello Shusan...."); */
+
+        /* Print print = new Print();
+        print.Data(39, "Lionel Messi", "Argentina"); */
+
+        /* Addition addition = new Addition();
+        addition.Add(6,5); */
+
+        /* Marks marks = new Marks();
+        marks.Points( 34, 55, 70 ); */
+
+        Circle circle = new Circle();
+        circle.Input(9.0, 5.0);
+
+
     }
 }
